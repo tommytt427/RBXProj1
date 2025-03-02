9060b3301c0817b839663a571551ed4716486bd6
tommytt427 <ttrann@umich.edu>
2025-03-02 18:47:51 -0500

    initial commit

--- src/ServerScriptService/Hello2.server.lua
+++
@@ -1,6 +0,0 @@
-local Workspace = game:GetService("Workspace")
-
-local Part = Workspace.Part
-
-local Part2 = Workspace.Testing.Part
-
--- src/ServerStorage/test.lua
+++
@@ -1,1 +0,0 @@
--- This is to test a two-way sync between Roblox Studio and Visual Studio Code
